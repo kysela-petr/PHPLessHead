8aa53e063b364097738ef6245db647cc0743afea
Petr Kysela <kysela.biz@gmail.com>
2013-02-27 19:17:11 +0100

    Create head.php
    read http://leafo.net/lessphp/

---
+++ head.php
@@ -0,0 +1,19 @@
+<?php 
+  $devServerHost = 'your-site.net';
+  $lastChangeOfAnyLessFile = md5(date("Y-m-d H:i:s", mktime(18, 52, 0, 2, 27, 2013)));
+  $lessCompiler = '<link rel="stylesheet/less" type="text/css" href="/css/style.less?'.$lastChangeOfAnyLessFile.'">
+                  <script src="/plugins/less-1.3.1.js" type="text/javascript"></script>';
+
+  if ( $_SERVER['HTTP_HOST'] == $devServerHost){ 
+    echo '<script type="text/javascript">less = { env: "development" };</script>' . $lessCompiler;
+  } else { 
+    require_once "./libs/lessphp/lessc.inc.php";
+    try {
+      $less = new lessc;
+      $less->checkedCompile("./css/style.less", "./web_temp/".$lastChangeOfAnyLessFile.".css" );
+      echo '<link rel="stylesheet" type="text/css" href="/web_temp/'.$lastChangeOfAnyLessFile.'.css" media="screen">';
+    } catch (Exception $e) {
+      echo $lessCompiler . '<script>console.log("less is not compiled");</script>';
+    }
+  } 
+?>
